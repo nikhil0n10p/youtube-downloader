d5ad3edc64ec2f6afe0201d51dadeb3a64171dc0
nikhil0n10p <nikumar8051@gmail.com>
2025-06-09 11:03:59 +0530

    Update tokens.py

--- config/tokens.py
+++ config/tokens.py
@@ -3,21 +3,21 @@ here you can define your own tokens
 """
 
 # place your bot token here
-bot_token: str = ""
+bot_token: str = "8056858217:AAGftmvS51VH-eodNPK0Q0ZXodXGpOPInGA"
 # place your bot username with @ here ex: @botfather
-bot_username: str = "@yourbotusername"
+bot_username: str = "@Idychcoihxudzxbot"
 # place your bot full link where its specified
-referral_link: str = "https://t.me/yourbotusername?start=ref_"
+referral_link: str = "https://t.me/Idychcoihxudzxbot?start=ref_"
 # place your admin user-id here ex: 12345, place multiple if you have more than one admin
-admin_id: set = {123}
+admin_id: set = {7060255401}
 # place your channel here full link here ex: https://t.me/yourchannelusername
-channel_link: str = "https://t.me/yourchannelusername"
+channel_link: str = "https://t.me/guugfgt8"
 # place your channel username with @ here ex: @telegram
-channel_username: str = "@yourchannelusername"
+channel_username: str = "@guugfgt8"
 # place your channel id here ex:-12345
-channel_id: int = -123
+channel_id: int = -1002772517385
 # place your support group id here ex:-1238124
-support_group_id: int = -123
+support_group_id: int = -4950309717
 
 # referral settings
 # if how many referrals the user adds they get a rewards ?
